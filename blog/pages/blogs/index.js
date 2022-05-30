ad81ea4e553b55e1a11fda38d6042a59daa109f3
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-05-30 14:21:36 +0530

    initial

---
+++ blog/pages/blogs/index.js
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import groq from 'groq'
+import client from '../../client'
+
+const Index = ({posts}) => {
+    return (
+      <div>
+        <h1>Welcome to a blog!</h1>
+        {posts.length > 0 && posts.map(
+          ({ _id, title = '', slug = '', publishedAt = '' }) =>
+            slug && (
+              <li key={_id}>
+                <Link href="/blogs/[slug]" as={`/blogs/${slug.current}`}>
+                  <a>{title}</a>
+                </Link>{' '}
+                ({new Date(publishedAt).toDateString()})
+              </li>
+            )
+        )}
+      </div>
+    )
+}
+
+export async function getStaticProps() {
+    const posts = await client.fetch(groq`
+      *[_type == "post" && publishedAt < now()]|order(publishedAt desc)
+    `)
+    console.log(posts)
+    return {
+      props: {
+        posts
+      }
+    }
+}
+
+export default Index
