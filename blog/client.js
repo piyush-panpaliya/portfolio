afe4f959e9d269cf24b269ffa115f55e42036bf1
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-05-30 18:39:15 +0530

    sanity client updated

--- blog/client.js
+++ blog/client.js
@@ -3,7 +3,7 @@ const sanityClient = require('@sanity/client')
 module.exports = sanityClient({
   projectId:"wl2bvo07" , // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
-  useCdn: true, // `false` if you want to ensure fresh data
+  useCdn: false, // `false` if you want to ensure fresh data
   apiVersion: '2021-12-28',
   ignoreBrowserTokenWarning: true
 })
