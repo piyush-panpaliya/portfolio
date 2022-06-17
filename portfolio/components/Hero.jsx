f17be2b29b1ab2251830c281ddd0dfe52b502553
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-06-18 00:00:32 +0530

    hero about work

--- portfolio/components/Hero.jsx
+++ portfolio/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className='flex flex-col items-center justify-between h-screen pt-[25vh] px-[10vw] gap-y-16'>
       <div className='w-full flex justify-between items-center ' >
         <div className='flex flex-col gap-y-3 max-w-[40vw]'>
-          <p className='text-4xl font-bold text-[#1E255E]'>Hey, I'm</p>
+          <p className='text-4xl font-bold text-[#1E255E]'>Hey, I&apos;m</p>
           <p className='text-7xl font-extrabold tracking-wide  mb-3 text-transparent bg-clip-text bg-gradient-to-r from-[#30A3EF] to-[#00568E] pt-1 pb-3'>Piyush Panpaliya</p>
           <p className='text-4xl font-bold text-[#1E255E] mb-2'>A Web Developer</p>
           <p className='text-xl font-medium text-[#414D56] max-w-[30rem]'>from India passionate about creating exceptional digital experiences</p>
@@ -40,7 +40,7 @@ const Stack = () => {
   return (
     <div className='flex px-4 gap-16'>
       {svg.map((a)=>
-      <img className='w-32' src={`/media/${a}`}/>
+      <img key={a} className='w-32' src={`/media/${a}`}/>
       )}
     </div>
   )
